f23253777e803c802f990182430cc20a35a015ec
byhugoleo <hugoleonardomachado@hotmail.com>
2022-05-11 11:41:52 -0300

    update main with new tests

--- database-tests/src/main/java/hugo/ifnmg/database/tests/Program.java
+++ database-tests/src/main/java/hugo/ifnmg/database/tests/Program.java
@@ -9,7 +9,14 @@ import hugo.ifnmg.database.tests.util.Util;
  */
 public class Program {
     public static void main(String[] args) {
+        //---------------- CPF TESTS ----------------//
         String CPF = "10012666688";
+        System.out.println(Util.isValidCPF(CPF));
+        
+        CPF = "11111111111";
+        System.out.println(Util.isValidCPF(CPF));
+        
+        CPF = "12345678912";
         System.out.println(Util.isValidCPF(CPF));
     }
 }

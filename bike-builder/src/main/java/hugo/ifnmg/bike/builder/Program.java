e8cf9324e8cc0d046092956226037be08c2ce83c
byhugoleo <hugoleonardomachado@hotmail.com>
2022-06-01 17:17:50 -0300

    add tests cases

--- bike-builder/src/main/java/hugo/ifnmg/bike/builder/Program.java
+++ bike-builder/src/main/java/hugo/ifnmg/bike/builder/Program.java
@@ -12,5 +12,23 @@ import hugo.ifnmg.bike.builder.builders.BikeBuilder;
 public class Program {
 
     public static void main(String[] args) {
+        Director director = new Director();
+        BikeBuilder builder = new BikeBuilder();
+        
+        director.constructBMXBike(builder);
+        Bike BMX = builder.getResult();
+        System.out.println(BMX);
+        
+        director.constructMountainBike(builder);
+        Bike mountain = builder.getResult();
+        System.out.println(mountain);
+        
+        director.constructSpeedBike(builder);
+        Bike speed = builder.getResult();
+        System.out.println(speed);
+        
+        director.constructUrbanBike(builder);
+        Bike urban = builder.getResult();
+        System.out.println(urban);
     }
 }

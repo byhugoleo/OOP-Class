1584dfcc0d05e6dd1760c1516002c1210d333448
byhugoleo <hugoleonardomachado@hotmail.com>
2022-06-01 17:08:53 -0300

    add constructs methods

--- bike-builder/src/main/java/director/Director.java
+++ bike-builder/src/main/java/director/Director.java
@@ -9,4 +9,39 @@ import hugo.ifnmg.bike.builder.builders.Builder;
  * @author byhugoleo
  */
 public class Director {
+    public void constructBMXBike(Builder builder) {
+        builder.setBikeType(BikeType.BMX);
+        builder.setColor("Red");
+        builder.setFrame();
+        builder.setGears();
+        builder.setWheels();
+        builder.setAcessories();
+    }
+    
+    public void constructMountainBike(Builder builder) {
+        builder.setBikeType(BikeType.MOUNTAIN);
+        builder.setColor("White");
+        builder.setFrame();
+        builder.setGears();
+        builder.setWheels();
+        builder.setAcessories();
+    }
+    
+    public void constructSpeedBike(Builder builder) {
+        builder.setBikeType(BikeType.SPEED);
+        builder.setColor("Black");
+        builder.setFrame();
+        builder.setGears();
+        builder.setWheels();
+        builder.setAcessories();
+    }
+    
+    public void constructUrbanBike(Builder builder) {
+        builder.setBikeType(BikeType.URBAN);
+        builder.setColor("Black");
+        builder.setFrame();
+        builder.setGears();
+        builder.setWheels();
+        builder.setAcessories();
+    }
 }

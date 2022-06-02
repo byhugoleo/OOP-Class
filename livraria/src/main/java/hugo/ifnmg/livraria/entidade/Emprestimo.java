c2844df7366de6ac677cdcace381cb4826bbb3b3
Xande <Xande@DESKTOP-638P6QR>
2022-06-02 16:45:13 -0300

    add classes emprestimo e funcionario

--- livraria/src/main/java/hugo/ifnmg/livraria/entidade/Emprestimo.java
+++ livraria/src/main/java/hugo/ifnmg/livraria/entidade/Emprestimo.java
@@ -1,9 +1,19 @@
 package hugo.ifnmg.livraria.entidade;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author byhugoleo
  */
 public class Emprestimo {
-
+    //private Funcionario funcionario;
+    //private Exemplar exemplar;
+    //private Discente discente;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+    private LocalDate dataDevolucaoLimite;
+    private String observacao;
+    
+    
 }

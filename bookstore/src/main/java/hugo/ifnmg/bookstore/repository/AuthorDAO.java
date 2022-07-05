0a41383ebc7de4d29777e15b5e39375482e58cef
byhugoleo <hugoleonardomachado@hotmail.com>
2022-07-05 16:03:42 -0300

    remove imports

--- bookstore/src/main/java/hugo/ifnmg/bookstore/repository/AuthorDAO.java
+++ bookstore/src/main/java/hugo/ifnmg/bookstore/repository/AuthorDAO.java
@@ -1,9 +1,6 @@
 package hugo.ifnmg.bookstore.repository;
 
 import hugo.ifnmg.bookstore.entity.Author;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
